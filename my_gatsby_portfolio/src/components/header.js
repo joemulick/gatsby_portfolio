d84b375037c9e10dbd7110f548dedb0149f38a8a
JoeMulick <joemulick@gmail.com>
2019-05-20 23:56:43 -0700

    added psuedo classes

--- my_gatsby_portfolio/src/components/header.js
+++ my_gatsby_portfolio/src/components/header.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-// import './header.module.scss'
-
 import headerStyles from './header.module.scss'
 
 const Header = () => {
     return (
         <header>
+            <h1>
+                <Link className={headerStyles.header} activeClassName={headerStyles.activeNavItem} to='/'>
+                Joe Mulick
+                </Link>
+            </h1>
             <nav>
-                <ul>
-                    <li><Link className={headerStyles.link} to='/'>home</Link></li>
-                    <li><Link to='/blog'>blog</Link></li>
-                    <li><Link to='/about'>about</Link></li>
-                    <li><Link to='/contact'>contact</Link></li>
+                <ul className={headerStyles.navList}>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>home</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/blog'>blog</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'>about</Link></li>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/contact'>contact</Link></li>
                 </ul>
             </nav>
         </header>
