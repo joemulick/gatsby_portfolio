642306ff2520c9d9d1d33e4b290f5a875447720e
JoeMulick <joemulick@gmail.com>
2019-05-20 23:16:55 -0700

    incorporated css modules

--- my_gatsby_portfolio/src/components/header.js
+++ my_gatsby_portfolio/src/components/header.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+// import './header.module.scss'
+
+import headerStyles from './header.module.scss'
+
 const Header = () => {
     return (
         <header>
             <nav>
                 <ul>
-                    <li><Link to='/'>home</Link></li>
+                    <li><Link className={headerStyles.link} to='/'>home</Link></li>
                     <li><Link to='/blog'>blog</Link></li>
                     <li><Link to='/about'>about</Link></li>
                     <li><Link to='/contact'>contact</Link></li>
--- my_gatsby_portfolio/src/components/layout.js
+++ my_gatsby_portfolio/src/components/layout.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import Footer from '../components/footer'
 import Header from '../components/header'
+
 import '../styles/index.scss'
+import layoutStyles from './layout.module.scss'
 
 const Layout = (props) => {
     return(
-        <div>
+        <div className={layoutStyles.container}>
             <Header />
                 {props.children}
             <Footer />
