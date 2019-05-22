8abc514244dd337d2c3c3680cf634befacc45d92
JoeMulick <joemulick@gmail.com>
2019-05-22 00:03:16 -0700

    markdown blogpost files rendering with graphql to the page

--- my_gatsby_portfolio/src/pages/blog.js
+++ my_gatsby_portfolio/src/pages/blog.js
@@ -1,13 +1,41 @@
 import React from "react"
+import { graphql, useStaticQuery } from 'gatsby'
 
 import Layout from '../components/layout'
 
 
 const BlogPage = () => {
+
+    const data = useStaticQuery(graphql`
+      query {
+          allMarkdownRemark {
+            edges {
+              node {
+                frontmatter {
+                  title
+                  date
+                }
+              }
+            }
+          }
+        }
+    `)
+
+    console.log(data)
+
     return (
         <Layout>
-            <h1>Blog</h1>
-            <p>Posts will show here later on.</p>
+
+            <ol>
+                <li>
+                    <h2>{data.allMarkdownRemark.edges[0].node.frontmatter.title}</h2>
+                    <p>{data.allMarkdownRemark.edges[0].node.frontmatter.date}</p>
+                </li>
+                <li>
+                    <h2>{data.allMarkdownRemark.edges[1].node.frontmatter.title}</h2>
+                    <p>{data.allMarkdownRemark.edges[1].node.frontmatter.date}</p>
+                </li>
+            </ol>
         </Layout>
     )
 }
