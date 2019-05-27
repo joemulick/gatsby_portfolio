f37a0e2918ae1881010dc93572de30751fc4abaf
JoeMulick <joemulick@gmail.com>
2019-05-27 13:33:03 -0700

    successfully renderd new page using dynamic slug with Gatsby Node APIs createPages method

--- my_gatsby_portfolio/gatsby-node.js
+++ my_gatsby_portfolio/gatsby-node.js
@@ -6,10 +6,51 @@ exports.onCreateNode = ({ node, actions }) => {
     if(node.internal.type === 'MarkdownRemark'){
 
         const slug = path.basename(node.fileAbsolutePath, '.md')
-        console.log('@@@@@@@@@@@@@@@@@', slug)
+        
+        createNodeField({
+            node,
+            name: 'slug',
+            value: slug
+        })
 
-        // console.log(JSON.stringify(node, undefined, 4))
+        // -- prints slug names to console console.log('@@@@@@@@@@@@@@@@@', slug)
+        // -- this console log lets you see the whole node object -- console.log(JSON.stringify(node, undefined, 4))
     }
 
+
+  }
+
+  module.exports.createPages = async ({ graphql, actions }) => {
+        const { createPage } = actions
+        const blogTemplate = path.resolve('./src/templates/blogTemplate.js')
+        
+        // This is a graphql method on 'Gatsby Node APIs' ~ Slightly different than graphql method on  ~ asynch method
+        const res = await graphql(`
+            query {
+                allMarkdownRemark {
+                    edges {
+                        node {
+                            fields {
+                                slug
+                            }
+                        }
+                    }
+                }
+            }
+        `)
+
+
+        res.data.allMarkdownRemark.edges.forEach((edge) => {
+            createPage({
+                component: blogTemplate,
+                path: `/blog/${edge.node.fields.slug}`,
+                context: {
+                    slug: edge.node.fields.slug
+                }
+            })
+        })
+        //1. Get path to template
+        //2. Get md data
+        //3. Create new pages
 
   }
