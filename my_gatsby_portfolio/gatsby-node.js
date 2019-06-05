0cdc6bafff04f8ccd633a45d707056a0dc9f2ce9
JoeMulick <joemulick@gmail.com>
2019-06-04 22:53:07 -0700

    images are rendering with contentful now

--- my_gatsby_portfolio/gatsby-node.js
+++ my_gatsby_portfolio/gatsby-node.js
@@ -1,32 +1,27 @@
 const path = require('path')
 
-  module.exports.createPages = async ({ graphql, actions }) => {
-        const { createPage } = actions
-        const blogTemplate = path.resolve('./src/templates/blogTemplate.js')
-        
-        // This is a graphql method on 'Gatsby Node APIs' ~ Slightly different than graphql method on  ~ asynch method
-        const res = await graphql(`
-            query {
-                allContentfulBlogPost {
-                    edges {
-                        node {
-                            slug
-                        }
+module.exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+    const blogTemplate = path.resolve('./src/templates/blogTemplate.js')
+    const res = await graphql(`
+        query {
+            allContentfulBlogPost {
+                edges {
+                    node {
+                        slug
                     }
                 }
             }
-        `)
+        }
+    `)
 
-
-        res.data.allContentfulBlogPost.edges.forEach((edge) => {
-            createPage({
-                component: blogTemplate,
-                path: `/blog/${edge.node.slug}`,
-                context: {
-                    slug: edge.node.slug
-                }
-            })
+    res.data.allContentfulBlogPost.edges.forEach((edge) => {
+        createPage({
+            component: blogTemplate,
+            path: `/blog/${edge.node.slug}`,
+            context: {
+                slug: edge.node.slug
+            }
         })
-
-
-  }
+    })
+}
