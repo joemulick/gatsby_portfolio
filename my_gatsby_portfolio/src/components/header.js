56ca1c37405b1a4890ccb1df375c66ea67cda953
JoeMulick <joemulick@gmail.com>
2019-06-11 21:05:23 -0700

    using container fluid - reworked footer using -https://css-tricks.com/couple-takes-sticky-footer/- calc()

--- my_gatsby_portfolio/src/components/header.js
+++ my_gatsby_portfolio/src/components/header.js
@@ -1,44 +1,73 @@
-import React from 'react'
-import { Link, graphql, useStaticQuery } from 'gatsby'
+import React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
-import headerStyles from './header.module.scss'
+import headerStyles from "./header.module.scss"
 
-import Navbar from 'react-bootstrap/Navbar'
-import { Nav } from 'react-bootstrap'
-
+import Navbar from "react-bootstrap/Navbar"
+import { Nav } from "react-bootstrap"
 
 const Header = () => {
-
-    const data = useStaticQuery(graphql`
-        query{
-            site {
-                siteMetadata {
-                    title
-                }
-            }
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
         }
-    `)
+      }
+    }
+  `)
 
-    return (
-        <header>
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand>
-                <Link className={headerStyles.navItem} to='/'>
-                    {data.site.siteMetadata.title}
-                </Link>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">                 
-                    </Nav>
-                    <Nav.Link><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/'>home</Link></Nav.Link>
-                    <Nav.Link><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/blog'>blog</Link></Nav.Link>
-                    <Nav.Link><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/about'>about</Link></Nav.Link>
-                    <Nav.Link><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to='/contact'>contact</Link></Nav.Link>  
-                </Navbar.Collapse>
-            </Navbar>
-        </header>
-    )
+  return (
+    <header>
+      <Navbar expand="md">
+        <Navbar.Brand>
+          <Link className={headerStyles.navItem} to="/">
+            {data.site.siteMetadata.title}
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto" />
+          <Nav.Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/"
+            >
+              home
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/blog"
+            >
+              blog
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/about"
+            >
+              about
+            </Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/contact"
+            >
+              contact
+            </Link>
+          </Nav.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  )
 }
 
 export default Header
--- my_gatsby_portfolio/src/components/layout.js
+++ my_gatsby_portfolio/src/components/layout.js
@@ -7,14 +7,23 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import '../styles/index.scss'
 import layoutStyles from './layout.module.scss'
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Layout = (props) => {
     return(
-        <div className={layoutStyles.container}>
-            <Header />
-                {props.children}            
-            <Footer />
-        </div>
+        <Container fluid>
+            <Row>
+                <Col lg={{ span: 10, offset: 1 }}>
+                    <div className={layoutStyles.content}>
+                    <Header />
+                    {props.children}
+                    </div>
+                    <div className={layoutStyles.footer}>
+                    <Footer />
+                    </div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
