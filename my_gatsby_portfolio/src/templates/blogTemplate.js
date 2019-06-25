839753d740221bcc4784368d633e7e2e0e5e86a9
JoeMulick <joemulick@gmail.com>
2019-06-24 22:15:29 -0700

    re-arranged blog post pages

--- my_gatsby_portfolio/src/templates/blogTemplate.js
+++ my_gatsby_portfolio/src/templates/blogTemplate.js
@@ -5,6 +5,8 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Layout from "../components/layout"
 import Head from "../components/head"
 
+import blogTemplateStyles from './blogTemplate.module.scss'
+
 export const query = graphql`
   query($slug: String!) {
     contentfulBlogPost(slug: { eq: $slug }) {
@@ -31,9 +33,45 @@ const Blog = props => {
   return (
     <Layout>
       <Head title={props.data.contentfulBlogPost.title}/>
-      <h1>{props.data.contentfulBlogPost.title}</h1>
-      <p>{props.data.contentfulBlogPost.publishedDate}</p>
-      {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
+        <div className={blogTemplateStyles.section_1}>
+          <h1 className={blogTemplateStyles.blogTitleStyle}>
+            {props.data.contentfulBlogPost.title}
+          </h1>
+          <div style={{textAlign:'center'}}>
+            <div className={blogTemplateStyles.authorImageContainer}>
+              <img style={{maxWidth:'65px',borderRadius:'50%'}} src="https://res.cloudinary.com/ddsihrmda/image/upload/v1510035228/mefiltered_qff7c0.jpg" alt="Joe Mulick" />
+            </div>
+            <div className={blogTemplateStyles.authorImageContainer}>
+              <p className={blogTemplateStyles.blogPostInfoStyle}>
+                <i>written by</i><br/>
+                <b>Joe Mulick</b>
+              </p>
+            </div>
+            <div className={blogTemplateStyles.authorImageContainer}>
+              <p className={blogTemplateStyles.blogPostInfoStyle}>
+                <i>published on</i><br/>
+                <b>{props.data.contentfulBlogPost.publishedDate}</b>
+              </p>
+            </div>
+            <div className={blogTemplateStyles.authorImageContainer}>
+                <div className={blogTemplateStyles.tempSocialIcons}>
+
+                </div>
+                <div className={blogTemplateStyles.tempSocialIcons}>
+
+                </div>
+                <div className={blogTemplateStyles.tempSocialIcons}>
+
+                </div>
+                <div className={blogTemplateStyles.tempSocialIcons}>
+
+                </div>
+            </div>
+          </div>
+          <p className={blogTemplateStyles.blogTextStyle}>
+            {documentToReactComponents(props.data.contentfulBlogPost.body.json, options)}
+          </p>
+        </div>
     </Layout>
   )
 }
